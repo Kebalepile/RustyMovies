469492fa01d18cc0ee01f20b2107d97735a01937
Kebalepile <pimpabutterfly86@gmail.com>
2020-11-26 12:29:06 +0200

    changed footer navebar to a toggle navbar

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,46 +1,61 @@
-import React, { useEffect, useRef, Fragment } from "react";
-import {PartialCss} from "./css/Css"
-import TitleCase from "../utils/TitleCase";
-
+import React, { useEffect, useRef, Fragment } from 'react';
+import { PartialCss } from './css/Css';
+import displayNav from '../../svg/enlarge.svg';
+import compressNav from '../../svg/compress.svg';
+import TitleCase from '../utils/TitleCase';
 
 const Footer = () => {
-    let display = useRef(null);
-    useEffect(() => {
-      // retrives available categorise from
-      // context.availableMS
-      let MS = ["Chat", "Playlist"];
-      for (let i of MS) {
-        let btn = document.createElement("button");
-        btn.name = i;
-        btn.textContent = TitleCase(i);
-        btn.addEventListener("click", Filter);
-        display.current.appendChild(btn);
-      }
-    });
-    /**
-     *
-     * @param {Event} e
-     * @description
-     * filters movies and series and displays the desired,
-     * category for the user.
-     */
-    const Filter = (e) => {
-      console.log("someone cliked me");
-      console.dir(e.target.name);
-    };
-    return (
-      <Fragment>
-        <div id="footerUiUx" >
-          <button> &#10094;</button>
-          <section id="filterMS" style={PartialCss} ref={display}></section>
-          <button> &#10095;</button>
-        </div>
-        
-      </Fragment>
-    );
-}
+	const addBtns = useRef(null),
+		controlBtn = useRef(null),
+		footerNavbar = useRef(null);
+	useEffect(() => {
+		// retrives available categorise from
+		// context.availableMS
+		let MS = ['Chat', 'Playlist'];
+		for (let i of MS) {
+			let btn = document.createElement('button');
+			btn.name = i;
+			btn.textContent = TitleCase(i);
+			btn.addEventListener('click', Filter);
+			addBtns.current.appendChild(btn);
+		}
+	});
+
+	const Close = (e) => {
+		controlBtn.current.style.display = 'block';
+		footerNavbar.current.style.display = 'none';
+		console.log('clicked');
+	};
+	const Open = (e) => {
+		controlBtn.current.style.display = 'none';
+		footerNavbar.current.style.display = 'block';
+		console.log('clicked');
+	};
+	/**
+	 *
+	 * @param {Event} e
+	 * @description
+	 * filters movies and series and displays the desired,
+	 * category for the user.
+	 */
+	const Filter = (e) => {
+		console.log('someone cliked me');
+		console.dir(e.target.name);
+	};
+	return (
+		<footer>
+			<img src={displayNav} ref={controlBtn} onClick={Open} title="chat and more..." className="footerCtrl" />
+			<div id="footerUiUx" ref={footerNavbar}>
+				<img src={compressNav} onClick={Close} className="footerCtrl" title="minimize chat and more ..." />
+				<section>
+					<button> &#10094;</button>
+					<section id="chat_and_playlist" style={PartialCss} ref={addBtns}></section>
+					<button> &#10095;</button>
+				</section>
+			</div>
+		</footer>
+	);
+};
 
 
-
-
-export default Footer
+export default Footer;
