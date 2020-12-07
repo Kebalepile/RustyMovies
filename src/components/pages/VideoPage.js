e722b95c1a3a705d87cfe99c512edd6c5ff8e4d0
Kebalepile <pimpabutterfly86@gmail.com>
2020-12-07 14:08:56 +0200

    created videopage component

---
+++ src/components/pages/VideoPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const VideoPage = () => {
+    return (
+        <div id="videopage">
+            <video 
+           src='#'
+            controls></video>
+        </div>
+    )
+}
+
+export default VideoPage
