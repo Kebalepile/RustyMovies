68ab61d730929bbe8c2890fd22599ef78e010e55
Kebalepile <pimpabutterfly86@gmail.com>
2020-12-09 10:31:59 +0200

    modified slider css

--- src/components/categories/css/Css.js
+++ src/components/categories/css/Css.js
@@ -1,11 +1,10 @@
 export const SlideShowCss = {
 	margin: 'auto',
-	width: '320px',
+	width: '350px',
 	overflowX: 'hidden',
 };
 
 export const SlideCss = {
-	width: '300px',
 	height: '200px',
 	margin: 'auto',
 	display: 'flex',
@@ -18,12 +17,11 @@ export const filmPicksCss = {
 	overflowX: 'auto',
 	overflowY: 'hidden',
 	height: '205px',
-	width: 'inherit',
 };
 
 export const imgCss = {
 	display: 'flex',
-
 	height: '30px',
 	cursor: 'pointer',
+	width: '30px'
 };
