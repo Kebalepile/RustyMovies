fdfc6b94502cb0f64bb36ca691f2bdf6c3cbde99
Kebalepile <pimpabutterfly86@gmail.com>
2023-08-14 15:34:32 +0200

    nah

--- scrapper.js
+++ scrapper.js
@@ -1,8 +1,11 @@
 import CimaTube from "./spiders/cimaTube.js";
 
+import { Trending, Recommended } from "./pipline/movieInfo.js";
 (async () => {
-  console.log("B-Town Cinema");
-  const spider = new CimaTube();
+  // await Trending();
+  // await Recommended();
+  // console.log("B-Town Cinema");
+  // const spider = new CimaTube();
   // daliy search
   // await spider.launch();
   // await spider.daliySearch();
