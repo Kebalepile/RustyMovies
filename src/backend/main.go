11047932b5f0050781cc6e3a490aae23283f9b9a
Kebalepile <pimpabutterfly86@gmail.com>
2023-10-20 10:31:47 +0200

    setup streamlord spider

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	streamlord "backend/spiders/streamlord"
+	"backend/types"
 	"log"
+	"sync"
 )
 
 func main() {
-	log.Println("hello")
+
+	log.Println("Movie info scrapper initiated.")
+	
+	streamlordSpider := streamlord.Spider{
+		Name: "streamlord",
+		AllowedDomains: []string{
+			"http://www.streamlord.com/index.html",
+		},
+	}
+
+	spiders := []types.Spider{
+		&streamlordSpider,
+	}
+
+	var wg sync.WaitGroup
+	for _, s := range spiders {
+		wg.Add(1)
+		go s.Init(&wg)
+	}
+	wg.Wait()
 }
