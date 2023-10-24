abc107c04480a81cc58d4bf72412787a66eceb5b
Kebalepile <pimpabutterfly86@gmail.com>
2023-10-24 13:43:20 +0200

    version 2 rusty biskop setup

--- settings.js
+++
@@ -1,8 +0,0 @@
-/**
- * @description puppeteer broswer settings.
- */
-export default {
-    headless: "new",
-    executablePath:
-      "C:\\Program Files (x86)\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
-  };
